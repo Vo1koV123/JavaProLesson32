0429e039f864b8428c8531832ba862290d129959
bodya <bodyanka229@gmail.com>
2023-07-12 15:35:41 +0300

    completed the task

--- src/test/java/com/example/glovodb/service/ProductServiceTest.java
+++ src/test/java/com/example/glovodb/service/ProductServiceTest.java
@@ -23,7 +23,7 @@ class ProductServiceTest {
         Product product = Product.builder()
                 .id(1)
                 .name("burger")
-                .cost(69.99)
+                .cost(49.99)
                 .orderid(123)
                 .build();
         Mockito.when(productRepository.save(product)).thenReturn(product);
